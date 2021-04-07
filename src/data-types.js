5e4e506afdcc81d3cda824558d1f4521f55c235f
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-07 13:51:28 +0300

    Implement code

--- src/data-types.js
+++ src/data-types.js
@@ -1,51 +1,73 @@
 function convert() {
-  // Student implementation
-  return;
+  let newArray = [];
+  for (let i = 0; i < arguments.length; i++) {
+    const element = arguments[i];
+    newArray[i] = typeof element === 'number' 
+      ? element.toString() 
+      : parseInt(element);
+  };
+
+  return newArray;
 }
 
-const executeforEach = () => {
-  // Student implementation
-  return;
+const executeforEach = (arr, callback) => {
+  arr.forEach(el => callback(el));
 };
 
-const mapArray = () => {
-  // Student implementation
-  return;
+const mapArray = (arr, callback) => {
+  let parseArr = arr.map(Number);
+
+  parseArr.forEach((el, index) => parseArr[index] = callback(el));
+
+  return parseArr;
 };
 
-const filterArray = () => {
-  // Student implementation
-  return;
+const filterArray = (arr, callback) => {
+  return arr.filter(el => callback(el));
 };
 
-const flipOver = () => {
-  // Student implementation
-  return;
+const flipOver = (str) => {
+  return str.split('').reverse().join('');
 };
 
-const makeListFromRange = () => {
-  // Student implementation
-  return;
+const makeListFromRange = (arr) => {
+  let result = [];
+
+  if(arr.length === 1)
+    result.push(arr[0]);
+
+  if (arr.length > 1) {
+    for (let i = arr[0]; i <= arr[1]; i++)
+      result.push(i);
+  }
+
+  return result;   
 };
 
-const getArrayOfKeys = () => {
-  // Student implementation
-  return;
+const getArrayOfKeys = (arr, key) => {
+  return arr.map(el => el[key]);
 };
 
-const substitute = () => {
-  // Student implementation
-  return;
+const substitute = (arr) => {
+  return arr.map(el => el < 30 ? '*' : el);
 };
 
-const getPastDay = () => {
-  // Student implementation
-  return;
+const getPastDay = (dt, numberOfDays) => {
+  const numberMsInDay = 1000 * 60 * 60 * 24;
+  return new Date(dt - numberOfDays * numberMsInDay).getDate()
 };
 
-const formatDate = () => {
-  // Student implementation
-  return;
+const formatDate = (dt) => {
+  var formatDtArr = [
+    '0' + (dt.getMonth() + 1),
+    '0' + dt.getDate(),
+    '0' + dt.getHours(),
+    '0' + dt.getMinutes()
+    ].map(el => el.slice(-2));
+
+  return dt.getFullYear().toString() + '/'
+    + formatDtArr.slice(0, 2).join('/') 
+    + ' ' + formatDtArr.slice(2).join(':');
 };
 
 module.exports = {
